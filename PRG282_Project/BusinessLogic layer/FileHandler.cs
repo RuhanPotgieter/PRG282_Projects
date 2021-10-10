9e80bd724e0592c23a9f7b69b58b956933714af4
Ruhan Potgieter <ruhan@sietechnn.co.za>
2021-10-10 18:48:47 +0200

    Adding custom message boxes into presentation layer

--- PRG282_Project/BusinessLogic layer/FileHandler.cs
+++ PRG282_Project/BusinessLogic layer/FileHandler.cs
@@ -7,16 +7,23 @@ using System.IO;
 
 namespace PRG282_Project.BusinessLogic_layer
 {
-    class FileHandler: Presentation_Layer.Login
+    class FileHandler: Presentation_Layer.Register
     {
         public static void Read()
         {
-            Presentation_Layer.Login l = new Presentation_Layer.Login();
+            Presentation_Layer.Register re = new Presentation_Layer.Register();
             StreamReader sr = new StreamReader(@"logininfo.txt");
             string olddata = sr.ReadLine();
             sr.Close();
             string[] data = new string[2];
-            data[0] = l.txt
+            data[0] = re.txt_UserName.Text;
+            data[1] = re.txt_Password.Text;
+            StreamWriter sw = new StreamWriter(@"logininfo.txt");
+            sw.WriteLine(olddata + (Environment.NewLine) + "Username=" + data[0] + " ,Password=" + data[1]);
+
+            sw.Close();
+
+
         }
 
     }
