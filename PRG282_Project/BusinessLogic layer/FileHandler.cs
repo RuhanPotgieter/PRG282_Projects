326e591ebdc612c8c53907e0f11bab8864d3381c
Ruhan Potgieter <ruhan@sietechnn.co.za>
2021-10-11 03:01:01 +0200

    edited filehandler

--- PRG282_Project/BusinessLogic layer/FileHandler.cs
+++ PRG282_Project/BusinessLogic layer/FileHandler.cs
@@ -7,12 +7,19 @@ using System.IO;
 
 namespace PRG282_Project.BusinessLogic_layer
 {
-    class FileHandler: Presentation_Layer.Register
+    class FileHandler
     {
         public static void Read()
         {
             try
             {
+                Presentation_Layer.Login l = new Presentation_Layer.Login();
+                StreamReader sr = new StreamReader(@"logininfo.txt");
+                string olddate = sr.ReadLine();
+                sr.Close();
+                string[] data = new string[2];
+                data[0] = l.txt_Username.Text;
+                data[1] = l.txt_Password.Text;
 
             }
             catch (customException)
